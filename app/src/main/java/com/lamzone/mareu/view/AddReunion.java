bab2c47a30f89ba99712aceec9e2e09ad35fd3b2
lleotraas <lleotraas@gmail.com>
2021-07-06 15:06:31 +0200

    un commit

---
+++ app/src/main/java/com/lamzone/mareu/view/AddReunion.java
@@ -0,0 +1,16 @@
+package com.lamzone.mareu.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by lleotraas on 06.
+ */
+public class AddReunion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
