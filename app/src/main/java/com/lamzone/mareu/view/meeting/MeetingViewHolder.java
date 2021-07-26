a901939a9e28852e157c4af34dc84b0bc1dbd4db
lleotraas <lleotraas@gmail.com>
2021-07-26 13:12:01 +0200

    ajout d'une initiale dans le cercle de couleur du row_meeting_list.xml nom des créateurs de réunion affiché en majuscule bordure des cadres arrondies

--- app/src/main/java/com/lamzone/mareu/view/meeting/MeetingViewHolder.java
+++ app/src/main/java/com/lamzone/mareu/view/meeting/MeetingViewHolder.java
@@ -18,6 +18,7 @@ public class MeetingViewHolder extends RecyclerView.ViewHolder {
     private final TextView mMember;
     private final ImageButton mDelete;
     private final ImageView mImage;
+    private final TextView mInitial;
 
     public MeetingViewHolder(View itemView) {
         super(itemView);
@@ -25,12 +26,14 @@ public class MeetingViewHolder extends RecyclerView.ViewHolder {
         mMember = itemView.findViewById(R.id.item_list_members_txt);
         mDelete = itemView.findViewById(R.id.item_list_delete_btn);
         mImage = itemView.findViewById(R.id.item_list_img);
+        mInitial = itemView.findViewById(R.id.item_list_initial_txt);
     }
 
     public void bind(Meeting meeting){
         mImage.setColorFilter(meeting.getColor());
-        mRoom.setText(String.format("Réunion %s - %sH%s - %s", meeting.getRoom(), meeting.getHour(), meeting.getMinute(), meeting.getName()));
+        mRoom.setText(String.format("Réunion %s - %sH%s - %s", meeting.getRoom(), meeting.getHour(), meeting.getMinute(), meeting.getName().toUpperCase()));
         mMember.setText(meeting.toString());
+        mInitial.setText(String.format("%s", Character.toUpperCase(meeting.getName().charAt(0))));
     }
 
     public ImageButton getDelete() {
